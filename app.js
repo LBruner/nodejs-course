b5b161ccf0dd1a4f61afa028551dce2b66e501ad
LBruner <leandrobruner15@gmail.com>
2022-06-01 09:23:43 -0300

    body parser (-> 09)

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.get('/home', (req,res)=>{
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.post('/home',(req,_)=>{
+    console.log(req.body)
+})
+
+app.get('/home', (req, res) => {
     res.send('OLA')
 })
 
 const port = 3000;
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`Started server on port ${port}`)
 })
